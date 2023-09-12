356f3f727dd2b57fbf3c90aeb3e4d8d04c37cf3d
Iryna Kutsevol <117863253+irkutsio@users.noreply.github.com>
2023-09-12 21:46:04 +0300

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -49,7 +49,7 @@ const verifyEmailControler = async (req, res) => {
 	const { verificationToken } = req.params;
 	const user = await User.findOne({ verificationToken });
 	if (!user) {
-		throw HttpError(401, 'Email not verify')
+		throw HttpError(404, 'User not found');
 	}
 
 	await User.findByIdAndUpdate(user._id, { verify: true, verificationToken: null });
